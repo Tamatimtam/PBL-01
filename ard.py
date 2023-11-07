67d9012dea1f4b57862b06917be3f79fa98fcb15
tamatimtam <pratama.varian.andika.parulian.tik22@mhsw.pnj.ac.id>
2023-11-07 13:09:11 +0700

    Added logging and real time LED status

--- ard.py
+++ ard.py
@@ -1,51 +1,41 @@
-#region Import necessary libraries
-from flask import Flask, render_template, request, session, redirect, url_for, jsonify  
+# Import necessary libraries
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_mysqldb import MySQL
 import requests
-import json     
 import bcrypt
 import os
-from requests.exceptions import ConnectionError  # Import ConnectionError
-#endregion
-
-#COBACOBA
+from requests.exceptions import ConnectionError
 
 # Create a Flask application
 app = Flask(__name__)
 
-#region Configure MySQL connection
-app.config['MYSQL_HOST'] = 'localhost'  # MySQL server host
-app.config['MYSQL_USER'] = 'root'   # MySQL username
-app.config['MYSQL_PASSWORD'] = ''  # MySQL password
-app.config['MYSQL_DB'] = 'user'  # MySQL database name
-#endregion
+# Configure MySQL connection
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'user'
 
-arduino_url = 'http://192.168.207.119'  # Arduino IP#
+arduino_url = 'http://192.168.71.105'
+
 # Initialize MySQL
 mysql = MySQL(app)
+
 # Secret key for session management
-app.secret_key = os.urandom(24) 
-
-
+app.secret_key = os.urandom(24)
 
 # Define a route for the registration page (only accessible to admins)
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    # Check if the user is an admin
     if 'logged_in' in session and session['role'] == 'admin':
         if request.method == 'POST':
-            # Get user input from the registration form
             username = request.form['txt']
             raw_password = request.form['pswd']
-            role = request.form['userType']  # You can set the role to 'admin' here if needed
+            role = request.form['userType']
 
-            # Hash the password before storing it in the database
             hashed_password = bcrypt.hashpw(raw_password.encode('utf-8'), bcrypt.gensalt())
 
-            # Connect to the MySQL database
             cur = mysql.connection.cursor()
 
-            # Check if the username already exists
             cur.execute("SELECT * FROM login_table WHERE username = %s", (username,))
             existing_user = cur.fetchone()
 
@@ -54,155 +44,157 @@ def register():
                 cur.close()
                 return render_template('register.html', error=error)
 
-            # If the username is unique, insert the new user into the database
             cur.execute("INSERT INTO login_table (username, password, role) VALUES (%s, %s, %s)",
                         (username, hashed_password, role))
             mysql.connection.commit()
 
-            # Close the database connection
             cur.close()
 
-            # Redirect to a success page or display a message
             success_message = 'User registered successfully'
             return render_template('register.html', success_message=success_message)
 
         return render_template('register.html')
 
-    # If the user is not an admin, deny access
-    error = 'Only Admins can register new user, please contact an admin!'
+    error = 'Only Admins can register new users, please contact an admin!'
     return render_template('loggedOut.html', error=error)
-
 
 # Define a route for the redirect to lamp or ac page
 @app.route('/menu')
 def menu():
-    # Check if the user is logged in
     if 'logged_in' in session:
         name = session['username']
         return render_template('loggedIn.html', name=name)
-        
-    
     error = 'Please log in!'
     return render_template('loggedOut.html', error=error)
-
-
-
 
 # Define a route for the redirect to lamp or ac page ADMIN VERSION
 @app.route('/menu_admin')
 def menu_admin():
-    # Check if the user is logged in
     if 'logged_in' in session and session['role'] == 'admin':
-        name = session['username']  
+        name = session['username']
         return render_template('loggedInAdmin.html', name=name)
-        
-    
     error = 'Please log in!'
     return render_template('loggedOut.html', error=error)
-
 
 # Define a route for the login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Get user input from the login form
         username = request.form['txt']
         password = request.form['pswd']
 
-        # Connect to the MySQL database
         cur = mysql.connection.cursor()
 
-        # Use a parameterized query to retrieve the user
         cur.execute("SELECT * FROM login_table WHERE username = %s", (username,))
         user = cur.fetchone()
 
-        # Close the database connection
         cur.close()
 
         if user is not None:
-            # Convert the stored hashed password from the database to bytes
-            hashed_password_from_db = user[2].encode('utf-8')  # Assuming 'password' is the third column in the table
-            
-            # Check if the input password matches the hashed password from the database
+            hashed_password_from_db = user[2].encode('utf-8')
+
             if bcrypt.checkpw(password.encode('utf-8'), hashed_password_from_db):
-                # If the user exists and the password is correct, create a session
                 session['logged_in'] = True
                 session['username'] = username
-                session['role'] = user[3]  # Assuming 'role' is the fourth column in the table
+                session['role'] = user[3]
+
                 if session['role'] == 'admin':
                     return redirect(url_for('menu_admin'))
                 return redirect(url_for('menu'))
 
-        # If the login is unsuccessful, show an error message
         error = 'Invalid login'
         return render_template('loggedOut.html', error=error)
 
-    # If it's a GET request, render the login form
     error = ''
     return render_template('loggedOut.html', error=error)
 
-
-
-
-
-
 # Define a route for the home page
-@app.route('/') 
+@app.route('/')
 def index():
-    # Check if the user is logged in
-    if 'logged_in' in session:
-        response_status = requests.get(arduino_url)
+    return render_template('loggedOut.html')
 
-        if response_status.status_code == 200:
-            led_status = response_status.text
-            return render_template('index.html', led_status=led_status)
-        else:
-            error_message = 'Failed to retrieve LED status'
-            return render_template('index.html', error_message=error_message)
-    else:
-        return redirect(url_for('login'))  # Redirect to the login page if not logged in
-    
-
-
-# Define a route for the home page
-@app.route('/lamp') 
+# Define a route for the lamp page
+@app.route('/lamp')
 def lamp():
-    # Check if the user is logged in
     if 'logged_in' in session:
         return render_template('lamp.html')
     else:
-        return redirect(url_for('login'))  # Redirect to the login page if not logged in
+        return redirect(url_for('login'))
+
+# Define a route for the logs page
+@app.route('/logs', methods=['GET', 'POST'])
+def view_logs():
+#    if 'logged_in' in session:
+        cur = mysql.connection.cursor()
+
+        cur.execute("SELECT username, timestamp, action FROM lamp_usage_reports")
+        logs = cur.fetchall()
+        cur.close()
+
+        logs_with_details = []
+
+        for log in logs:
+            username = log[0]
+            timestamp = log[1]
+            action = log[2]
+
+            logs_with_details.append({
+                'username': username,
+                'timestamp': timestamp,
+                'action': action
+            })
+
+        return render_template('logs.html', logs=logs_with_details)
+    #else:
+     #   return redirect(url_for('login'))
 
 # Define a route to log out
 @app.route('/logout')
 def logout():
-    # Remove the session variables to log out
     session.pop('logged_in', None)
     session.pop('username', None)
     session.pop('role', None)
     return redirect(url_for('login'))
+
     
 # Define route for LED Status
 @app.route('/get_led_status', methods=['GET'])
 def get_led_status():
     try:
-        response_status = requests.get(f'{arduino_url}/get_servo_position')
+        response_status = requests.get(f'{arduino_url}/get_led_status')
         response_status.raise_for_status()  # Raise an exception for HTTP errors
-        servo_position = int(response_status.text)
-        led_status = "ON" if servo_position == 180 else "OFF"
-        return led_status
+        led_status = response_status.text
+        if led_status == 'On':
+            led_info = 'Lampu Nyala!'
+        elif led_status == 'Off' :
+            led_info = 'Lampu Mati!'
+        else:
+            led_info = f'ERROR!, {led_status}'
+        return led_info
     except ConnectionError as e:
         return 'unknown'  # Handle connection errors
 
 # Define a route to turn on the LED
-@app.route('/turn_on_led', methods=['POST', 'GET'   ])
+@app.route('/turn_on_led', methods=['POST', 'GET'])
 def turn_on_led():
     # Check if the user is logged in
     if 'logged_in' in session:
         # Send a request to NodeMCU to turn on the LED
-        requests.get(f'{arduino_url}/turn_on_led')  # NodeMCU IP
+        response = requests.get(f'{arduino_url}/turn_on_led')  # NodeMCU IP
+        username = session['username']
+        if response.text == "LED turned on":
+            action = "Turn on LED"
+        else:
+            action = "Turn off LED"
+
+        # Insert a record into the lamp_usage_reports table
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO lamp_usage_reports (username, timestamp, action) VALUES (%s, NOW(), %s)", (username, action))
+        mysql.connection.commit()
+        cur.close()
     else:
-        return redirect(url_for('login'))  
+        return redirect(url_for('login'))
+
 
 
 
